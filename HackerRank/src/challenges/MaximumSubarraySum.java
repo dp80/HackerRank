d3ab94137aab7c351a599843978f4acd529c65ab
dhiren-patil <dhiren.patil@hp.com>
2016-09-06 10:17:45 -0500

    add

--- HackerRank/src/challenges/MaximumSubarraySum.java
+++ HackerRank/src/challenges/MaximumSubarraySum.java
@@ -11,16 +11,9 @@ public class MaximumSubarraySum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		//int querynum = scan.nextInt();
+		int querynum = scan.nextInt();
 		
-		String s = "saveChangesInTheEditor";
-		String[] arr = s.split("[A-Z]");
-		System.out.println(arr.length);
 		
-		System.out.println(s);
-		for(int i = 0; i < arr.length; i++){
-			System.out.println(arr[i]);
-		}
 	}
 
 }
