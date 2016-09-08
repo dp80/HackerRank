57b1d6bc0f48a15f63740030eb5a6535c573955b
dhiren-patil <dhiren.patil@hp.com>
2016-09-08 10:00:00 -0500

    summaryRange problem commit

---
+++ HackerRank/src/challenges/SummaryRanges.java
@@ -0,0 +1,59 @@
+package challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] n = {1,9};
+		SummaryRanges sr = new SummaryRanges();
+		List<String> ll = new ArrayList<String>();
+		ll = sr.summaryRanges(n);
+		
+		for(String str: ll){
+			System.out.print(str+",");
+		}
+	}
+	
+	List<String> summaryRanges(int[] nums) {
+        String value = "";
+        ArrayList<String> l = new ArrayList<String>();
+        if(nums.length == 1)
+            l.add(Integer.toString(nums[0]));
+        else if(nums.length == 0){
+        	return l;
+        }else{
+        	int number;
+        	for(int i = 0; i < nums.length-1; i++){
+        		if(i == 0){
+        			value = Integer.toString(nums[0]);
+        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 == (nums[i+1])){
+        			continue;
+        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 != (nums[i+1])){
+        			value = value + "->"+nums[i];
+        			l.add(value);
+        			value = "";
+        		}else if(nums[i] != (nums[i-1] + 1) && nums[i]+1 == nums[i+1]){
+        			value = Integer.toString(nums[i]);
+        		}else if(nums[i] != nums[i-1]+1 && nums[i]+1 != (nums[i+1])){
+        			value = Integer.toString(nums[i]);
+        			l.add(value);
+        			value = "";
+        		}
+        		if ((i+1) == nums.length - 1){
+        			if(value != "" )
+        				l.add(value + "->"+nums[i+1]);
+        			else
+        				l.add(Integer.toString(nums[i+1]));
+        		}
+        	}
+        }
+        
+        return l;
+	}
+
+}
+	 
