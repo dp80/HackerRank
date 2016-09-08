fb6bddfa99aabf2be3c09c91854e4f75a8b813bf
dhiren-patil <dhiren.patil@hp.com>
2016-09-08 10:42:41 -0500

    fails the test case [1,9]

--- HackerRank/src/challenges/SummaryRanges.java
+++ HackerRank/src/challenges/SummaryRanges.java
@@ -8,7 +8,7 @@ public class SummaryRanges {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] n = {1,9};
+		int [] n = {0,1};
 		SummaryRanges sr = new SummaryRanges();
 		List<String> ll = new ArrayList<String>();
 		ll = sr.summaryRanges(n);
@@ -19,40 +19,42 @@ public class SummaryRanges {
 	}
 	
 	List<String> summaryRanges(int[] nums) {
-        String value = "";
-        ArrayList<String> l = new ArrayList<String>();
-        if(nums.length == 1)
-            l.add(Integer.toString(nums[0]));
-        else if(nums.length == 0){
-        	return l;
-        }else{
-        	int number;
-        	for(int i = 0; i < nums.length-1; i++){
-        		if(i == 0){
-        			value = Integer.toString(nums[0]);
-        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 == (nums[i+1])){
-        			continue;
-        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 != (nums[i+1])){
-        			value = value + "->"+nums[i];
-        			l.add(value);
-        			value = "";
-        		}else if(nums[i] != (nums[i-1] + 1) && nums[i]+1 == nums[i+1]){
-        			value = Integer.toString(nums[i]);
-        		}else if(nums[i] != nums[i-1]+1 && nums[i]+1 != (nums[i+1])){
-        			value = Integer.toString(nums[i]);
-        			l.add(value);
-        			value = "";
-        		}
-        		if ((i+1) == nums.length - 1){
-        			if(value != "" )
-        				l.add(value + "->"+nums[i+1]);
-        			else
-        				l.add(Integer.toString(nums[i+1]));
-        		}
-        	}
-        }
-        
-        return l;
+		 String value = "";
+	        List<String> l = new ArrayList<String>();
+	        if(nums.length == 1)
+	            l.add(Integer.toString(nums[0]));
+	        else if(nums.length == 0){
+	        	return l;
+	        }else{
+	        	for(int i = 0; i < nums.length-1; i++){
+	        		if(i == 0){
+	        			value = Integer.toString(nums[0]);
+	        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 == (nums[i+1])){
+	        			continue;
+	        		}else if(nums[i] == (nums[i-1] + 1) && nums[i]+1 != (nums[i+1])){
+	        			value = value + "->"+nums[i];
+	        			l.add(value);
+	        			value = "";
+	        		}else if(nums[i] != (nums[i-1] + 1) && nums[i]+1 == nums[i+1]){
+	        			value = Integer.toString(nums[i]);
+	        		}else if(nums[i] != nums[i-1]+1 && nums[i]+1 != (nums[i+1])){
+	        			value = Integer.toString(nums[i]);
+	        			l.add(value);
+	        			value = "";
+	        		}
+	        		if ((i+1) == nums.length - 1){
+	        		    if( value != "" && nums[i] != nums[i+1] ){
+	        		        l.add(value);
+	        		        l.add(Integer.toString(nums[i+1]));
+	        		    }else if(value != ""){
+	        				l.add(value + "->"+nums[i+1]);
+	        			}else
+	        				l.add(Integer.toString(nums[i+1]));
+	        		}
+	        	}
+	        }
+
+	        return l;
 	}
 
 }
